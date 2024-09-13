2760bb34cd85d6b8b820cb10f7b8f4c70c6b37e4
Miriam <miriamsobrinodev@gmail.com>
2024-09-13 22:38:43 +0200

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "var(--bg)",
+        primary: "var(--primary)",
+        green: "var(--green)",
+        pink: "var(--pink)",
+        orange: "var(--orange)",
+        blue: "var(--blue)",
+        violet: "var(--violet)"
+      },
+    },
+  },
+  plugins: [],
+};
